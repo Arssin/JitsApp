f5a934d9aa69a39e048903ed10e54574ae32cd2c
Arssin <mateusz.binieda@onet.pl>
2022-03-30 16:31:20 +0200

    Add font, 1st background colors

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from '../Footer/Footer.module.scss';
+
+export function Footer() {
+  return(
+    <div className={styles.footer}>
+      Footer
+    </div>
+  )
+}
---
+++ src/Header/Header.jsx
@@ -0,0 +1,7 @@
+import styles from '../Header/Header.module.scss'
+
+export function Header(){
+  return (
+    <div className={styles.welcomeBar}>JitsApp - your rolling space</div>
+  )
+}
