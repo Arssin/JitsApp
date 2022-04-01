4ad84d8df761de9fdacc1c5bc88098232e0cf384
Arssin <mateusz.binieda@onet.pl>
2022-04-01 16:08:24 +0200

    chaning homeview

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,8 +3,11 @@ import styles from '../Home/Home.module.scss';
 export function HomePage() {
   return (
     <div className={styles.home}>
-      <div className={styles.homeText}>Witaj na stronie głównej Przybyszu!</div>
-      <div>
+      <div className={styles.homeText}>
+        Witaj na stronie! lorem ipsum dolor sit amet, consectetur adipiscinglorem ipsum dolor sit amet, consectetur
+        adipiscinglorem ipsum dolor sit amet, consectetur adipiscinglorem ipsum dolor sit amet, consectetur
+      </div>
+      <div className={styles.homeDivBtn}>
         <button className={styles.homeBtn}>ClickMe</button>
       </div>
     </div>
