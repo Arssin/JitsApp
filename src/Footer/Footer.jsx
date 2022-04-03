a7495ec64bfa5335b6f5a7062fd88f45e7a9f69a
Arssin <mateusz.binieda@onet.pl>
2022-04-03 21:23:06 +0200

    Layout changes, add social icons, add logo

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import styles from '../Footer/Footer.module.scss';
+import pic from '../images/JitsApp.png';
+import ig from '../images/instagram.svg';
+import fb from '../images/facebook.svg';
 
 export function Footer() {
-  return(
+  return (
     <div className={styles.footer}>
-      Footer
+      <img src={pic} alt="logo" className={styles.logo} />
+      <h3 className={styles.footerText}>Made by Mateusz Binięda</h3>
+      <a href="www.instagram.com" className={styles.logosAnchors}>
+        <img src={ig} alt="instagram logo" />
+      </a>
+      <a href="www.facebook.com" className={styles.logosAnchors}>
+        <img src={fb} alt="facebook logo" />
+      </a>
     </div>
-  )
+  );
 }
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,11 @@
 import styles from '../Header/Header.module.scss';
+import pic from '../images/JitsApp.png';
 
 export function Header() {
   return (
     <div className={styles.welcomeBar}>
-      <h1 className={styles.fontDiv}>JitsApp - your rolling space</h1>
+      <img className={styles.logo} alt="logo" src={pic} />
+      <h1 className={styles.fontDiv}> - Your rolling space</h1>
     </div>
   );
 }
