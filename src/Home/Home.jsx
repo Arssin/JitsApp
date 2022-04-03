5a1409de5aeff69c7e996dc83d810b5131b5b974
Arssin <mateusz.binieda@onet.pl>
2022-04-03 21:40:11 +0200

    Test Text

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,9 +5,9 @@ export function HomePage() {
     <div className={styles.home}>
       <div className={styles.homeText}>
         <div className={styles.welcomeText}>Witaj na JitsApp! </div>
-        Jest to aplikacja, która ma na celu ułatwić ludziom trenującym Brazylijskie Jiu-Jitsu, osiągnaie wymarzonego
+        Jest to aplikacja, która ma na celu ułatwić ludziom trenującym Brazylijskie Jiu-Jitsu i osiągnie wymarzonego
         progressu. Zapominasz technik? Nie odnotowałeś kolejnych zawodów? Czy może jesteś po prostu ciekawski? - tutaj
-        znajdziesz wszystko czego potrzebujesz!
+        znajdziesz wszystko czego potrzebujesz! TEKST TESTOWY
       </div>
       <div className={styles.homeDivBtn}>
         <a href="#" className={styles.homeBtn} data-back="Ossssss!" data-front="Przejdź do aplikacji"></a>
